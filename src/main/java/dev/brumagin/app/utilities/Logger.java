0d1c1706d357839f743d932b8845c1a7fee48656
David Brumagin <dbrumagin@gmail.com>
2022-04-21 23:32:54 -0400

    change log path

--- src/main/java/dev/brumagin/app/utilities/Logger.java
+++ src/main/java/dev/brumagin/app/utilities/Logger.java
@@ -25,7 +25,7 @@ public class Logger {
     public static void log(String message, LogLevel level){
 
         String logMessage = level.name() +" " +  message + " " + new Date() + "\n";
-        String path = "";
+        String path = System.getProperty("user.dir")+"//expenses.log";
 
         try {
             File file = new File(path);
