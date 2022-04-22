ad76c2334c2e03e07c4afe2795f7e7ffc6e67265
David Brumagin <dbrumagin@gmail.com>
2022-04-21 23:28:37 -0400

    fixed auto-testing in maven

--- src/main/java/dev/brumagin/app/utilities/Logger.java
+++ src/main/java/dev/brumagin/app/utilities/Logger.java
@@ -1,5 +1,6 @@
 package dev.brumagin.app.utilities;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -24,14 +25,16 @@ public class Logger {
     public static void log(String message, LogLevel level){
 
         String logMessage = level.name() +" " +  message + " " + new Date() + "\n";
-        String path = "C:\\Users\\dbrum\\IdeaProjects\\Project1\\src\\main\\java\\dev\\brumagin\\app\\expenses.log";
+        String path = "";
 
         try {
+            File file = new File(path);
+            boolean write = file.createNewFile(); // attempt to create the file if not found
+
             Files.write(Paths.get(path),
                     logMessage.getBytes(StandardCharsets.UTF_8),
                     StandardOpenOption.APPEND);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             //e.printStackTrace();
         }
 
