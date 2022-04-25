3b3144e68fedb9c6530a7da61a6c4f94e954157d
David Brumagin <dbrumagin@gmail.com>
2022-04-25 09:10:47 -0400

    ORM update / rough createEntity method

--- src/main/java/dev/brumagin/app/data/CrudDAO.java
+++ src/main/java/dev/brumagin/app/data/CrudDAO.java
@@ -1,14 +1,8 @@
 package dev.brumagin.app.data;
 
-import dev.brumagin.app.entities.Column;
 import dev.brumagin.app.entities.PrimaryKey;
 import dev.brumagin.app.utilities.ConnectionUtility;
-import dev.brumagin.app.utilities.LogLevel;
-import dev.brumagin.app.utilities.Logger;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedType;
-import java.lang.reflect.Field;
+import java.lang.reflect.*;
 import java.sql.*;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,96 +10,94 @@ import java.util.stream.Collectors;
 
 public interface CrudDAO <T> {
     default T createEntity(T entity) {
-      //try {
-        Connection connection = ConnectionUtility.createConnection();
-        List<Field> fields = Arrays.stream(entity.getClass().getDeclaredFields()).collect(Collectors.toList());
-        System.out.println(Arrays.stream(fields.get(0).getAnnotations()).findFirst().get().annotationType().getName());
-        System.out.println(PrimaryKey.class.getName());
-        for(Field f : fields){
-            System.out.println(f);
-        }
-        fields = fields.stream().filter(n -> Arrays.stream(n.getAnnotations()).findFirst().get().annotationType().getName().equals(PrimaryKey.class.getName())).collect(Collectors.toList());
-        for(Field f : fields){
-            System.out.println(f);
-        }
-        List<String> editedSQLColumns= new ArrayList<>();
-        StringBuilder conversionString;
-        for (Field f : fields){
-            conversionString = new StringBuilder();
-            char[] convert = f.getName().toCharArray();
-            for(char c : convert){
-                if(Character.isUpperCase(c)){
-                    conversionString.append("_");
-                    conversionString.append(Character.toLowerCase(c));
-                }
-                else {
-                    conversionString.append(c);
-                }
-            }
-            editedSQLColumns.add(String.valueOf(conversionString));
-        }
+      try {
+          Connection connection = ConnectionUtility.createConnection();
+          List<Field> fields = Arrays.stream(entity.getClass().getDeclaredFields())
+                  .collect(Collectors.toList());
+          List<Field> fields2 = fields.stream()
+                  .filter(n -> Arrays.stream(n.getAnnotations()).findFirst().get().annotationType().getName().equals(PrimaryKey.class.getName()))
+                  .collect(Collectors.toList());
+          fields = fields.stream()
+                  .filter(n -> !Arrays.stream(n.getAnnotations()).findFirst().get().annotationType().getName().equals(PrimaryKey.class.getName()))
+                  .collect(Collectors.toList());
+          List<String> editedSQLColumns = new ArrayList<>();
+          StringBuilder conversionString;
 
-        //System.out.println(entity.getClass().getName().substring(26).toLowerCase());
-        StringBuilder statement = new StringBuilder("insert into ");
-        statement.append(entity.getClass().getName().substring(entity.getClass().getPackage().getName().length()+1).toLowerCase());
-        statement.append(" (");
-        for (int i = 0; i < fields.size(); i++) {
-           // Optional<Annotation> optionalAnnotation = Arrays.stream(fields[i].getAnnotations()).findFirst();
-            //System.out.println(optionalAnnotation.get().annotationType().getName().substring(26));
-            if(i+1< fields.size()){
-            statement.append(editedSQLColumns.get(i)); //(fields[i].getName()+",");
-            statement.append(", ");}
-            else {
-                statement.append( editedSQLColumns.get(i));//(fields[i].getName()+") ");
-                statement.append(") ");
-            }
-        }
-        statement.append("values (");
-        System.out.println(statement);
+          for (Field f : fields) {
+              conversionString = new StringBuilder();
+              char[] convert = f.getName().toCharArray();
+              for (char c : convert) {
+                  if (Character.isUpperCase(c)) {
+                      conversionString.append("_");
+                      conversionString.append(Character.toLowerCase(c));
+                  } else {
+                      conversionString.append(c);
+                  }
+              }
+              editedSQLColumns.add(String.valueOf(conversionString));
+          }
 
+          StringBuilder statement = new StringBuilder("insert into ");
+          statement.append(entity.getClass().getName().substring(entity.getClass().getPackage().getName().length() + 1).toLowerCase());
+          statement.append(" (");
+          for (int i = 0; i < fields.size(); i++) {
+              if (i + 1 < fields.size()) {
+                  statement.append(editedSQLColumns.get(i));
+                  statement.append(", ");
+              } else {
+                  statement.append(editedSQLColumns.get(i));
+                  statement.append(") ");
+              }
+          }
+          statement.append("values (");
+          List<Method> methods = Arrays.stream(entity.getClass().getMethods()).collect(Collectors.toList());
+          List<Method> method;
+          List<Method> methods2 = new ArrayList<>();
+
+          for (int i = 0; i < fields.size(); i++) {
+              String convert = Character.toUpperCase(fields.get(i).getName().charAt(0)) + fields.get(i).getName().substring(1);
+              method = methods.stream().filter(n -> n.getName().equals("get" + convert)).collect(Collectors.toList());
+              methods2.add(method.get(0));
+              if (i + 1 < fields.size())
+                  statement.append(" ?,");
+              else {
+                  statement.append(" ?);");
+              }
+          }
+
+          PreparedStatement ps = connection.prepareStatement(statement.toString(), Statement.RETURN_GENERATED_KEYS);
+          for (int i = 0; i < fields.size(); i++) {
+              Type type = methods2.get(i).getReturnType();
+              System.out.println(type.getTypeName());
+              if (type.getTypeName().equals("java.lang.String"))
+                  ps.setString(i + 1, (String) methods2.get(i).invoke(entity));
+              else {
+                  System.out.println("other type");
+              }
+          }
+          ps.execute();
+          ResultSet rs = ps.getGeneratedKeys();
+          rs.next();
+          conversionString = new StringBuilder();
+          char[] convert2 = fields2.get(0).getName().toCharArray();
+          for (char c : convert2) {
+              if (Character.isUpperCase(c)) {
+                  conversionString.append("_");
+                  conversionString.append(Character.toLowerCase(c));
+              } else {
+                  conversionString.append(c);
+              }
+          }
+          int generatedKey = rs.getInt(String.valueOf(conversionString));
+      }
+
+      catch (SQLException e){
+          System.out.println("Thrown SQL");
+      } catch (InvocationTargetException e) {
+          e.printStackTrace();
+      } catch (IllegalAccessException e) {
+          e.printStackTrace();
+      }
         return entity;
     }
 }
-            /*String statement = "insert into employee (first_name,last_name) values (?,?);";
-
-            PreparedStatement ps = connection.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, employee.getFirstName());
-            ps.setString(2, employee.getLastName());
-            ps.execute();
-            ResultSet rs = ps.getGeneratedKeys();
-            rs.next();
-            int generatedKey = rs.getInt("employee_id");
-            employee.setId(generatedKey);
-
-            return employee;*/
-       // }
-        //catch (SQLException e){
-           // Logger.log("**The employee was not created; please check database access and parameters.**\n"+employee,LogLevel.WARNING);
-        //    return null;
-       // columns.forEach(System.out::println);
-
-        /*
-            String statement = "update employee set first_name = ?, last_name = ? where employee_id = ?;";
-            PreparedStatement ps = connection.prepareStatement(statement);
-            ps.setString(1, employee.getFirstName());
-            ps.setString(2, employee.getLastName());
-            ps.setInt(3, employee.getId());
-            return ps.executeUpdate() != 0;
-        }
-        catch (SQLException e){
-            Logger.log("**The employee was not found; please check database access and parameters.**\n" + employee, LogLevel.WARNING);
-            return false;
-        }*/
-   // } // creates an entity
-    /*default T getEntityById(int id){
-
-    }; // Get an entity by ID
-
-    default List<T> getAllEntities(){
-
-    };// get all instances of the entity
-
-    default T updateEntity(T entity){
-
-    };// update an instance*/
-
